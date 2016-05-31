58d7b7a73db52aa637f9189d18e1d132ac13506d
NeiloRV <rvneilo@gmail.com>
2016-05-31 22:31:32 +0300

    additional start window and several comments were added

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,19 +1,25 @@
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class MainClass {
 	final static int port = 8181;
+	String choosenSide;
+	boolean serverStarted = false;
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in); 
+		//Scanner in = new Scanner(System.in); 
 		 
-	    System.out.println("Which side of CHAT do you want to run: server (s) or client (c)"); 
-	    while (true) { 
-	        char answer = Character.toLowerCase(in.nextLine().charAt(0)); 
-	        if (answer == 's') { 
+	  //  System.out.println("Which side of CHAT do you want to run: server (s) or client (c)"); 
+		
+		MainClass neiloChat = new MainClass();
+		StartSorCWindow startWindow = new StartSorCWindow(neiloChat);
+	    while (true) {
+	    	if(neiloChat.choosenSide != null){
+	     //   char answer = Character.toLowerCase(in.nextLine().charAt(0)); 
+	        if (neiloChat.choosenSide.equals("Server")) { 
 	            new Server(port); 
 	            break; 
-	        } else if (answer == 'c') { 
+	        } else if (neiloChat.choosenSide.equals("Client")) { 
 	            System.out.println("new client was created");
 	        	//new Client(port); 
 	           WindowClient newClient = new WindowClient();
@@ -21,7 +27,8 @@ public class MainClass {
 	        } else { 
 	            System.out.println("Uncorect data. Please try again"); 
 	        } 
+	    }
 	    } 
-	    in.close();
+	  //  in.close();
 	} 
 }
--- src/Server.java
+++ src/Server.java
@@ -11,20 +11,26 @@ public class Server {
 
 	ServerSocket server;
 	Socket socket;
-	Connection connection;
-	ArrayList<Connection> connections = new ArrayList<>();
+	Connection connection;  // new client connection
+	ArrayList<Connection> connections = new ArrayList<>();  // list of accepted connections
 
-	public Server(int port) {
+	/* 
+	 * Next block create start server side of chat
+	 * and waiting for new client connection
+	 */
+	public Server(int port) {    
 		try {
-			server = new ServerSocket(port);
-			System.out.println("new server was created");
+			// next line starts server on port
+			server = new ServerSocket(port);  
+			
+			//System.out.println("new server was created");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 		while (true) {
 			try {
-				socket = server.accept();
+				socket = server.accept();  // 
 				Connection connection = new Connection(socket);
 				connections.add(connection);
 			} catch (IOException e) {
@@ -33,23 +39,27 @@ public class Server {
 		}
 	}
 
+	/*
+	 * Next block describe private class of connection
+	 * which creates for any new client
+	 */
 	private class Connection extends Thread {
-		private BufferedReader in;
-		private PrintWriter out;
-		private Socket socket;
-		private String name = "";
+		private BufferedReader in;   // input stream
+		private PrintWriter out;	// output stream
+		private Socket socket;		// socket of connection
+		private String name = "";	// nic name of new client
 
 		public Connection(Socket inSocket) {
 
 			socket = inSocket;
 			try {
-				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				name = in.readLine();
-				out = new PrintWriter(socket.getOutputStream(), true);
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream())); // create input stream
+				name = in.readLine();													// save name of new client
+				out = new PrintWriter(socket.getOutputStream(), true); 	// create output stream
 				
 				for(Connection c : connections){
-					out.println("%%" + c.name);  // send the name of other connections to new client
-					c.out.println("%%"+name);  		// send new client name to other connected client
+					out.println("%%" + c.name);  // send to new client the name of other clients (for adress-book)
+					c.out.println("%%"+name);  		// send new client name to other connected client (for salute)
 				}
 				
 				start();
@@ -63,6 +73,9 @@ public class Server {
 			}
 		}
 
+		/* 
+		 * next block organize message exchanging beetween clients
+		 */
 		public void run() {
 			try {
 				for (Connection c : connections) {
@@ -76,13 +89,18 @@ public class Server {
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
-					if (str != null) {
+					if (str != null) {  // only valuable String is allowed
+						
+						// if exit was accept - so, client has gone;
+						// next block sends message about it
 						if (str.equals("exit")) {
 							for (Connection c : connections) {
 								c.out.println(name + " has left chat.");
 							}
 							break;
 						}
+						
+						// next block sends message to all connected clients
 						for (Connection c : connections) {
 							c.out.println(name + ": " + str);
 						}
